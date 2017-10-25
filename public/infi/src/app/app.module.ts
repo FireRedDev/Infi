2cd9e81631fdca3214f7ab4d647b0ed561e1855f
Isabella Hundstorfer <isabella.hundstorfer@gmail.com>
2017-10-25 12:06:28 +0200

    add sidebar and edit styled.css

--- public/infi/src/app/app.module.ts
+++ public/infi/src/app/app.module.ts
@@ -6,19 +6,44 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'angular-calendar';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserService } from './user.service';
+import {AuthguardGuard} from "./authguard.guard";
+import { SidebarModule } from 'ng-sidebar';
 
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginFormComponent
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthguardGuard],
+    component: DashboardComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-	BrowserAnimationsModule,
-	FormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
     HttpModule,
-    CalendarModule.forRoot()
+    RouterModule.forRoot(appRoutes),
+    CalendarModule.forRoot(),
+    SidebarModule.forRoot()
   ],
-  providers: [],
+  providers: [UserService, AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ public/infi/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,124 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user.service";
+import {
+  CalendarEvent,
+  CalendarDateFormatter,
+  DAYS_OF_WEEK
+} from 'angular-calendar';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  viewDate: Date = new Date();
+  events = [];
+  title = 'app';
+  locale: string = 'de';
+  view: string = 'month';
+  weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
+  weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
+
+  private _opened: boolean = false;
+  private _modeNum: number = 0;
+  private _positionNum: number = 0;
+  private _dock: boolean = false;
+  private _closeOnClickOutside: boolean = false;
+  private _closeOnClickBackdrop: boolean = false;
+  private _showBackdrop: boolean = false;
+  private _animate: boolean = true;
+  private _trapFocus: boolean = true;
+  private _autoFocus: boolean = true;
+  private _keyClose: boolean = false;
+  private _autoCollapseHeight: number = null;
+  private _autoCollapseWidth: number = null;
+
+  private _MODES: Array<string> = ['over', 'push', 'slide'];
+  private _POSITIONS: Array<string> = ['left', 'right', 'top', 'bottom'];
+
+  private _toggleOpened(): void {
+    this._opened = !this._opened;
+  }
+
+  private _toggleMode(): void {
+    this._modeNum++;
+
+    if (this._modeNum === this._MODES.length) {
+      this._modeNum = 0;
+    }
+  }
+
+  private _toggleAutoCollapseHeight(): void {
+    this._autoCollapseHeight = this._autoCollapseHeight ? null : 500;
+  }
+
+  private _toggleAutoCollapseWidth(): void {
+    this._autoCollapseWidth = this._autoCollapseWidth ? null : 500;
+  }
+
+  private _togglePosition(): void {
+    this._positionNum++;
+
+    if (this._positionNum === this._POSITIONS.length) {
+      this._positionNum = 0;
+    }
+  }
+
+  private _toggleDock(): void {
+    this._dock = !this._dock;
+  }
+
+  private _toggleCloseOnClickOutside(): void {
+    this._closeOnClickOutside = !this._closeOnClickOutside;
+  }
+
+  private _toggleCloseOnClickBackdrop(): void {
+    this._closeOnClickBackdrop = !this._closeOnClickBackdrop;
+  }
+
+  private _toggleShowBackdrop(): void {
+    this._showBackdrop = !this._showBackdrop;
+  }
+
+  private _toggleAnimate(): void {
+    this._animate = !this._animate;
+  }
+
+  private _toggleTrapFocus(): void {
+    this._trapFocus = !this._trapFocus;
+  }
+
+  private _toggleAutoFocus(): void {
+    this._autoFocus = !this._autoFocus;
+  }
+
+  private _toggleKeyClose(): void {
+    this._keyClose = !this._keyClose;
+  }
+
+  private _onOpenStart(): void {
+    console.info('Sidebar opening');
+  }
+
+  private _onOpened(): void {
+    console.info('Sidebar opened');
+  }
+
+  private _onCloseStart(): void {
+    console.info('Sidebar closing');
+  }
+
+  private _onClosed(): void {
+    console.info('Sidebar closed');
+  }
+  
+  constructor(private user:UserService) { }
+
+  ngOnInit() {
+  }
+
+}
+
+
